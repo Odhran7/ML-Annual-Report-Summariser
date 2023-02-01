c2eca75d2d9516a8af4a5b6495f0694e4e813cf2
Odhran7 <russell.odhran@gmail.com>
2023-02-01 20:54:42 +0000

    Change open function to append

--- Text-Extraction/pdftotxt.py
+++ Text-Extraction/pdftotxt.py
@@ -9,11 +9,26 @@ text_file = open('AAPL.txt', 'w', encoding="utf-8")
 for page in pdf:
     try:
         text = page.get_text()
-        text_file.write(text) # Error here -> Not writing to file... some buffer issue
+        text_file.write(text) 
         print(text)
     except IOError as error:
         print(error)
 
+text_file.close()
+
+# Write function to give paths
+
 # Write function here to convert pdf to text -> 
 
-text_file.close()
+def convert_pdf_txt(path, output):
+    pdf = fitz.open(path)
+    text = ""
+    output_file = open('', 'a', encoding = 'utf-8') # Need to define ticker
+    for page in pdf:
+        try:
+            text = page.get_text()
+            output_file.writw(text)
+        except IOError:
+            print(error)
+    output_file.close()
+
