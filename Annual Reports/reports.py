4a09f8aab7eed297b6396e5b93a6e76558f1f4cb
james26572 <102800087+james26572@users.noreply.github.com>
2023-02-01 21:19:05 +0000

    loaderfunctions

--- Annual Reports/reports.py
+++ Annual Reports/reports.py
@@ -4,75 +4,78 @@ from sandp500_tickers import save_sp500_tickers
 from sec_api import QueryApi
 import json 
 
-api_key = "b8113d497aa775cb50186f7f03c97c7e8b3158734705787264d31e7f627dc6db"
+def get_10k_URLS_for_particular_company(start, end,ticker):
+  api_key = "161be08b7c1b9d92a04867c50069b622edcc8716ee151ae6929c571d9e3689c3"
 
-tickers = save_sp500_tickers()
+  #tickers = save_sp500_tickers()
 
 # Creating a new instance of the queryapi 
 
-queryapi = QueryApi(api_key)
+  
 
-base_query = {
-  "query": { 
-      "query_string": { 
-          "query": "PLACEHOLDER", # this will be set during runtime 
-          "time_zone": "America/New_York"
-      } 
-  },
+  queryApi = QueryApi(api_key)
+
+  query = {
+  "query": { "query_string": { 
+      "query": "ticker:AAPL AND filedAt:[2020-01-01 TO 2021-12-31] AND formType:\"10-K\"",
+      "time_zone": "America/New_York"
+  } },
   "from": "0",
-  "size": "200", # dont change this
-  # sort returned filings by the filedAt key/value
+  "size": "10",
   "sort": [{ "filedAt": { "order": "desc" } }]
 }
-# open the file we use to store the filing URLs
-log_file = open("filing_urls.txt", "a")
 
-# start with filings filed in 2022, then 2020, 2019, ... up to 1995
-# uncomment next line to fetch all filings filed from 2022-1995
-# for year in range(2021, 1994, -1):
-for year in range(2022, 2020, -1):
-  print("Starting download for year {year}".format(year=year))
+  response = queryApi.get_filings(query)
+
+  
+  
+
+  log_file = open("filing_urls.txt", "a")
+
+# start with filings filed in 2021, then 2020, 2019, ... up to 2010 
+# uncomment line below to fetch all filings filed in 2022-2010
+# for year in range(2021, 2009, -1):
+  for year in range(start, end, -1):
+    print("starting {year}".format(year=year))
   
   # a single search universe is represented as a month of the given year
-  for month in range(1, 13, 1):
+    
     # get 10-Q and 10-Q/A filings filed in year and month
     # resulting query example: "formType:\"10-Q\" AND filedAt:[2021-01-01 TO 2021-01-31]"
     universe_query = \
-        "formType:(\"10-K\", \"10-KT\", \"10KSB\", \"10KT405\", \"10KSB40\", \"10-K405\") AND " + \
-        "filedAt:[{year}-{month:02d}-01 TO {year}-{month:02d}-31]" \
-        .format(year=year, month=month)
+    "ticker:{ticker} AND "+ \
+    "filedAt:[{year}-01 TO {year}-31] AND" \
+    "formType:\"10-K\""
+    universe_query.format(ticker = ticker)
+    universe_query.format(year=year)
   
+    print(universe_query)
     # set new query universe for year-month combination
-    base_query["query"]["query_string"]["query"] = universe_query
+    query["query"]["query_string"]["query"] = universe_query;
 
     # paginate through results by increasing "from" parameter 
     # until we don't find any matches anymore
-    # uncomment next line to fetch all 10,000 filings
+    # uncomment line below to fetch 10,000 filings
     # for from_batch in range(0, 9800, 200): 
-    for from_batch in range(0, 400, 200):
-      # set new "from" starting position of search 
-      base_query["from"] = from_batch;
-
-      response = queryapi.get_filings(base_query)
-
-      # no more filings in search universe
-      if len(response["filings"]) == 0:
-        break
+    
 
       # for each filing, only save the URL pointing to the filing itself 
       # and ignore all other data. 
       # the URL is set in the dict key "linkToFilingDetails"
-      urls_list = list(map(lambda x: x["linkToFilingDetails"], response["filings"]))
+    urls_list = list(map(lambda x: x["linkToFilingDetails"], response["filings"]))
 
       # transform list of URLs into one string by joining all list elements
       # and add a new-line character between each element.
-      urls_string = "\n".join(urls_list) + "\n"
+    urls_string = "\n".join(urls_list) + "\n"
       
-      log_file.write(urls_string)
+    log_file.write(urls_string)
 
-    print("Filing URLs downloaded for {year}-{month:02d}".format(year=year, month=month))
 
-log_file.close()
+
+
+  log_file.close()
+
+get_10k_URLS(2022,2021,"AAPL")
 
 import os
 import multiprocessing
