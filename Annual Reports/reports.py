2f86caca61cdcbe0c7bfeb50e4bdda3b89510ca9
james26572 <102800087+james26572@users.noreply.github.com>
2023-02-04 17:37:49 +0000

    functions for extracting item + preprocessing data

--- Annual Reports/reports.py
+++ Annual Reports/reports.py
@@ -6,15 +6,7 @@ import json
 
 def get_10k_URLS_for_particular_company(start, end,ticker):
   api_key = "b8113d497aa775cb50186f7f03c97c7e8b3158734705787264d31e7f627dc6db"
-
-  #tickers = save_sp500_tickers()
-
-# Creating a new instance of the queryapi 
-
-  
-
   queryApi = QueryApi(api_key)
-
   query = {
   "query": { "query_string": { 
       #"query": "ticker:AAPL AND filedAt:[2020-01-01 TO 2020-12-31] AND formType:\"10-K\"",
@@ -25,54 +17,33 @@ def get_10k_URLS_for_particular_company(start, end,ticker):
   "size": "10",
   "sort": [{ "filedAt": { "order": "desc" } }]
 }
-
-  
-
-  
-  
-
   log_file = open("filing_urls.txt", "a")
   
 
-# start with filings filed in 2021, then 2020, 2019, ... up to 2010 
-# uncomment line below to fetch all filings filed in 2022-2010
-# for year in range(2021, 2009, -1):
+
   for year in range(start, end, -1):
     print("starting {year}".format(year=year))
   
-  # a single search universe is represented as a month of the given year
-    
-    # get 10-Q and 10-Q/A filings filed in year and month
-    # resulting query example: "formType:\"10-Q\" AND filedAt:[2021-01-01 TO 2021-01-31]"
+  
     universe_query = "ticker:{ticker} AND ".format(ticker = ticker)+\
     "filedAt:[{year}-01-01 TO {year}-12-31] AND ".format(year = year)+\
     "formType:\"10-K\"" 
     
     
   
-    print(universe_query)
-    # set new query universe for year-month combination
+    
+    
     query["query"]["query_string"]["query"] = universe_query;
-    print(query)
+    
     response = queryApi.get_filings(query)
-    #print(response)
-
-    # paginate through results by increasing "from" parameter 
-    # until we don't find any matches anymore
-    # uncomment line below to fetch 10,000 filings
-    # for from_batch in range(0, 9800, 200): 
     
 
-      # for each filing, only save the URL pointing to the filing itself 
-      # and ignore all other data. 
-      # the URL is set in the dict key "linkToFilingDetails"
-    urls_list = list(map(lambda x: x["linkToFilingDetails"], response["filings"]))
-    #print(urls_list[-1])
+    
+    
 
-      # transform list of URLs into one string by joining all list elements
-      # and add a new-line character between each element.
-    #urls_string = "\n".join(urls_list) + "\n"
-    #print(urls_string)
+      
+    urls_list = list(map(lambda x: x["linkToFilingDetails"], response["filings"]))
+    
       
     log_file.write(urls_list[-1]+"\n")
 
@@ -83,26 +54,16 @@ def get_10k_URLS_for_particular_company(start, end,ticker):
 
 
 
-get_10k_URLS_for_particular_company(2020,2018,"TSLA")
+get_10k_URLS_for_particular_company(2022,2000,"TSLA")
 
-import os
-import multiprocessing
 
-def download_all_filings():
-  print("Start downloading all filings")
 
-  download_folder = "./filings" 
-  if not os.path.isdir(download_folder):
-    os.makedirs(download_folder)
-    
-  # uncomment next line to process all URLs
-  # urls = load_urls()
-  urls = load_urls()[1:40]
-  print("{length} filing URLs loaded".format(length=len(urls)))
-
-  number_of_processes = 20
-
-  with multiprocessing.Pool(number_of_processes) as pool:
-    pool.map(download_filing, urls)
-  
-  print("All filings downloaded")
+def get_section(path, item): 
+    file = open(path, 'r')
+    lines = file.readlines()
+    output_file = open(path[:-3] + "_{item}.txt".format(item = item), 'a')
+    for url in lines:
+      print(url)
+      section_text = extractorApi.get_section(url, item, "text")
+      output_file.write(section_text + "\n\n\n")
+    output_file.close()
