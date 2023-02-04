160d82cdd29f62ffb01cd0a32f1930261a82aad7
Odhran7 <russell.odhran@gmail.com>
2023-02-04 23:18:27 +0000

    Update function

--- Annual Reports/reports.py
+++ Annual Reports/reports.py
@@ -29,7 +29,7 @@ base_query = {
 # This function writes 10k filing urls   for ticker = ticker to ticker.txt from start_year to end_year
 
 def get_urls_for_ticker(base_query, ticker, start_year, end_year):
-  log_file = open('{ticker}.txt'.format(ticker = ticker), 'a')
+  log_file = open('./urls/{ticker}_urls.txt'.format(ticker = ticker), 'a')
   for year in range(end_year, start_year, -1):
     print('Starting {year}'.format(year = year))
     universe_query = "ticker:{ticker} AND ".format(ticker = ticker) + \
@@ -46,45 +46,3 @@ def get_urls_for_ticker(base_query, ticker, start_year, end_year):
     print("Filing URLs downloaded for {year}-01".format(year=year))
   log_file.close()
 
-
-
-''' 
-1 - Business
-1A - Risk Factors
-1B - Unresolved Staff Comments
-2 - Properties
-3 - Legal Proceedings
-4 - Mine Safety Disclosures
-5 - Market for Registrant’s Common Equity, Related Stockholder Matters and Issuer Purchases of Equity Securities
-6 - Selected Financial Data (prior to February 2021)
-7 - Management’s Discussion and Analysis of Financial Condition and Results of Operations
-7A - Quantitative and Qualitative Disclosures about Market Risk
-8 - Financial Statements and Supplementary Data
-9 - Changes in and Disagreements with Accountants on Accounting and Financial Disclosure
-9A - Controls and Procedures
-9B - Other Information
-10 - Directors, Executive Officers and Corporate Governance
-11 - Executive Compensation
-12 - Security Ownership of Certain Beneficial Owners and Management and Related Stockholder Matters
-13 - Certain Relationships and Related Transactions, and Director Independence
-14 - Principal Accountant Fees and Services
-'''
-# Getting the clean-text of an item (see above) for the list of urls in a .txt file located at path
-
-def get_section(path, item): 
-    file = open(path, 'r')
-    lines = file.readlines()
-    output_file = open(path[:-3] + "_{item}.txt".format(item = item), 'a')
-    for url in lines:
-      print(url)
-      section_text = extractorApi.get_section(url, item, "text")
-      output_file.write(section_text + "\n\n\n")
-    output_file.close()
-
-# Testing above function 
-
-# Getting just the 10ks for TSLA from the year 2022 to 2000
-
-# get_urls_for_ticker(base_query, 'TSLA', 2000, 2022)
-
-get_section('./TSLA.txt', '7')
