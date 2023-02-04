33e45f0b2aaad294715ab47e1a1b49dd53c567e5
Odhran7 <russell.odhran@gmail.com>
2023-02-04 23:18:38 +0000

    Add execute file

---
+++ Annual Reports/run.py
@@ -0,0 +1,26 @@
+from pre_process import remove_stopwords_and_tokenize, annual_report_items, get_text
+from reports import base_query, get_urls_for_ticker
+from sandp500_tickers import save_sp500_tickers
+from sec_api import QueryApi
+from sec_api import ExtractorApi
+
+# Setting API keys
+
+api_key = "b8113d497aa775cb50186f7f03c97c7e8b3158734705787264d31e7f627dc6db"
+
+# Getting all the tickers from the S and P 500
+
+tickers = save_sp500_tickers()
+
+# Creating a new instance of the queryapi & extractorApi
+
+queryapi = QueryApi(api_key)
+extractorApi = ExtractorApi(api_key)
+
+# Getting all annual reports for s and p 500
+
+for ticker in tickers:
+    get_urls_for_ticker(base_query, ticker, 2000, 2022)
+    path = './urls/' + ticker + '_urls.txt'
+    get_text(path, annual_report_items)
+
