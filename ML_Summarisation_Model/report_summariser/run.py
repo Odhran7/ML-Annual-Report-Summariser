1fd9f07cd745ade97f8aad1a589071207e7785c2
Odhran7 <russell.odhran@gmail.com>
2023-02-21 22:45:07 +0000

    Set up run environment

--- ML_Summarisation_Model/report_summariser/run.py
+++ ML_Summarisation_Model/report_summariser/run.py
@@ -1,4 +1,5 @@
 from report_summariser import load_data
+from Pegasus_finetune import train
 
 # Import the dataset
 huggingface_dict = load_data()
@@ -6,6 +7,6 @@ huggingface_dict = load_data()
 
 # Trains the model
 
-# trainer = train()
+trainer = train()
 
 # Calculate the metrics
