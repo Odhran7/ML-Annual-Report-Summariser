bc41be6707720368383d888888ab9f6fa617ee18
james26572 <102800087+james26572@users.noreply.github.com>
2023-02-26 03:02:30 +0000

    data set creation

--- ML_Summarisation_Model/Training/make_dataset.py
+++ ML_Summarisation_Model/Training/make_dataset.py
@@ -1,9 +1,9 @@
 import os
 from datasets import Dataset
-
+import pandas as pd
 # This function creates a Hugging Face dataset with cols id, document, summary whereby summaries are the labels. It returns the train/test subsets @ 0.8. It only does this for the first 10 annual reports
 
-def load_data():
+def load_data1():
     train_data = []
     label_summaries = []
 
@@ -47,4 +47,23 @@ def load_data():
     
     return dataset_train_test
 
+
+
+
+def loadData():
+    df = pd.read_csv('ML_Summarisation_Model\Training\Summaries\Item 1A - Risk Factors\A\A_Item_1A_Document_Summary - Sheet1.csv', header=None, names=["row", 'document', 'summary'],encoding="utf-8")
+    print(df.head())
+    texts = df["document"].astype(str)
+    summaries = df["summary"].astype(str)
+    ids = df['row'].astype(str)
+    data_dict = {"id":ids,"documents":texts,"summaries":summaries}
+    
+    
+    dataset = Dataset.from_dict(data_dict)
+    dataset = dataset.train_test_split(train_size=0.8,)
+    
+
+    return dataset
+dataset = loadData()
+print(dataset)
 
